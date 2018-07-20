13af0b05b4767880773e125a2e0e1c73a1d72ba8
mrjeffcodes95 <mrjeffcodes95@gmail.com>
2018-07-20 15:44:12 -0500

    updated

--- c2p_pipe.c
+++ c2p_pipe.c
@@ -4,38 +4,38 @@
 #include <sys/types.h>
 
 //set buffer size for arrays in pre.c
-#define BUFSIZE 100
+//#define BUFSIZE 100
 
 
 main()
 {
- char inbuf[BUFSIZE];
+ //char inbuf[BUFSIZE];
  int p[2], j;
  pid_t pid;
  
  if (pipe(p) == -1)
  {
-  printf("pipe\n");
   perror("\npipe call failed");
   exit(1);
  }
  
  switch(pid = fork()) {
  case -1: 
-       printf("fork\n");
        perror("\nfork call ");
        exit(2);
  case 0:
+       close(1);
        dup(p[1]);
+       close(p[1]);
        close(p[0]);
-       printf("pre\n");
        execl("./pre", "pre",(char *)0);
        perror("\nexecl() pre failed\n");
        break;
  default:
+       close(0);
        dup(p[0]);
        close(p[1]);
-       //printf("sort\n");
+       close(p[0]);
        execl("./sort", "sort", (char *)0);
        perror("\nexecl() sort failed\n");
   }
